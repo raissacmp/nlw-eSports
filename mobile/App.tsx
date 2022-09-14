b93be7596c99b9d65a9185fc449a507321535e53
Raissa Campos <raissa.campos26@gmail.com>
2022-09-14 20:58:23 -0300

    feat: interface splash and home mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,32 @@
 import { Background } from "./src/components/Background";
+import { StatusBar } from 'react-native'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+
+import { Home } from './src/screens/Home'
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
-  return <Background></Background>;
+
+ const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
+
+  return <Background>
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
+    {fontsLoaded ?  <Home/> : <Loading/>   }
+
+  </Background>;
 }
