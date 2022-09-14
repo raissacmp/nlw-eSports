26ad735d54fbe40ea8be931e0fce05143aedd749
Raissa Campos <raissa.campos26@gmail.com>
2022-09-13 21:45:34 -0300

    feat: initial mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Background } from "./src/components/Background";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text> React Native</Text>
-      <StatusBar style="auto"/>
-    </View>
-  );
+  return <Background></Background>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,17 @@
+import { ImageBackground } from "react-native";
+
+import backgroundImage from "../../assets/background-galaxy.png";
+
+import { styles } from "./styles";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground source={backgroundImage} defaultSource={backgroundImage} style={styles.container}>
+      {children}
+    </ImageBackground>
+  );
+}
