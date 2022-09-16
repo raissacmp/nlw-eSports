b6b146a36a07b90ccbc6dc4e519646b9375778d0
Raissa Campos <raissa.campos26@gmail.com>
2022-09-16 20:02:25 -0300

    feat: scruture database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,22 @@
-import express from 'express';
+import express from "express";
 
-const app = express ()
+const app = express();
 
-app.get('/ads', (request, response) => {
- return response.json([
-    { id: 1, name: 'Anuncio 1'},
-    { id: 2, name: 'Anuncio 2'},
-    { id: 3 , name: 'Anuncio 3 '},
-    { id: 4 , name: 'Anuncio 4 '}
- ])
-})
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
 
-app.listen(3333)
+app.post("/ads", (request, response) => {
+  return response.json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => { // : se refere a dado dinamico
+  //listar games do id X os seus anuncios
+
+// const adId = request.params.id;
+
+
+  return response.json([]);
+});
+
+app.listen(3333);
