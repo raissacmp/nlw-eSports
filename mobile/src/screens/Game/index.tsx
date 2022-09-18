d8d37c1e3e6b2ae30febf14e05532f3262f23c51
Raissa Campos <raissa.campos26@gmail.com>
2022-09-18 16:55:10 -0300

    feat initial content page game

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,17 +1,75 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Background } from "../../components/Background";
-import { useRoute } from "@react-navigation/native";
-
-import { styles } from "./styles";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { GameParams } from "../../@types/navigation";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+import { Heading } from "../../components/Heading";
+import { Background } from "../../components/Background";
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { THEME } from "../../theme";
+import { styles } from "./styles";
 
 export function Game() {
+  const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
 
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <Background>
-      <SafeAreaView style={styles.container}></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={24}
+            />
+          </TouchableOpacity>
+
+          <Image source={logoImg} style={styles.logo} />
+
+          <View style={styles.right} />
+        </View>
+
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
+
+        {/* <FlatList
+          data={duos}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <DuoCard
+              data={item}
+              onConnect={() => getDiscordUser(item.id)}
+            />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent ]}
+          showsHorizontalScrollIndicator
+          ListEmptyComponent={
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados ainda.
+            </Text>
+          }
+        />
+
+        <DuoMatch
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
+        /> */}
+      </SafeAreaView>
     </Background>
   );
 }
